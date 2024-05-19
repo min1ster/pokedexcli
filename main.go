bb2e5a8e34be7a81123fa56ed2b44b53b512265c
min1ster <min1ster>
2024-05-19 03:15:49 -0400

    Attempt to get a couple of commands working. One for 'help' and one for 'exit'

--- main.go
+++ main.go
@@ -2,8 +2,60 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"os"
 )
 
+type cliCommand struct {
+	name string
+	description string
+	callback func() error
+}
+
+func getCommands() map[string]cliCommand {
+	commands := make(map[string]cliCommand)
+
+	helpCommand := cliCommand{
+		name: "help",
+		description: "Displays a help message",
+		callback: func() error {
+			fmt.Print("\nWelcome to Pokedex!\n\n")
+			for command := range commands {
+				fmt.Printf("%s: %s\n", commands[command].name, commands[command].description)
+			}
+			fmt.Println()
+			return nil
+		},
+	}
+
+	exitCommand := cliCommand{
+		name: "exit",
+		description: "Exit the Pokedex",
+		callback: func() error {
+			os.Exit(0)
+			return nil
+		},
+	}
+
+	commands["help"] = helpCommand
+	commands["exit"] = exitCommand
+
+	return commands
+}
+
 func main() {
-	fmt.Println("Hello world")
+	commands := getCommands()
+	for {
+		fmt.Print("Pokedex > ")
+		scanner := bufio.NewScanner(os.Stdin)
+		if scanner.Scan() {
+			input := scanner.Text()
+			command, ok := commands[input]
+			if !ok {
+				fmt.Println("Unknown command")
+				continue
+ 			}
+			command.callback()
+		}		
+	}
 }
