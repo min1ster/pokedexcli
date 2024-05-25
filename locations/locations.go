61827526da185be0e2e7383d73a314942e261eb0
min1ster <min1ster>
2024-05-25 03:08:56 -0400

    Modularize the endpoint handling logic to keep things DRY

--- locations/locations.go
+++ locations/locations.go
@@ -27,43 +27,25 @@ type locationPayload struct {
 	} `json:"pokemon_encounters"`
 }
 
+type Callback func(bodyBytes []byte) error
+
 func GetLocations(page int, cache *pokecache.Cache) error {
 	offset := 20 * page
 	endpoint := fmt.Sprintf("https://pokeapi.co/api/v2/location-area?offset=%d&limit=20", offset)
-	cacheEntry, ok := cache.Entries[endpoint]
-	if ok {
-		cache.Add(endpoint, cacheEntry.Val)
-		return handleLocationsOutput(cacheEntry.Val)
-	}
-
-	res, err := http.Get(endpoint)
-
-	if err != nil {
-		log.Fatal(err)
-		return err
-	}
-
-	defer res.Body.Close()
-
-	if res.StatusCode != http.StatusOK {
-		log.Fatalf("Response failed with status code: %d", res.StatusCode)
-	}
-
-	bodyBytes, err := io.ReadAll(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	cache.Add(endpoint, bodyBytes)
-	return handleLocationsOutput(bodyBytes)
+	return endointHandler(cache, endpoint, handleLocationsOutput)
 }
 
 func GetLocation(name string, cache *pokecache.Cache) error {
 	fmt.Printf("Exploring %s...\n", name)
 	endpoint := fmt.Sprintf("https://pokeapi.co/api/v2/location-area/%s", name)
+	return endointHandler(cache, endpoint, handleLocationOutput)
+}
+
+func endointHandler(cache *pokecache.Cache, endpoint string, callback Callback) error {
 	cacheEntry, ok := cache.Entries[endpoint]
 	if ok {
 		cache.Add(endpoint, cacheEntry.Val)
-		return handleLocationOutput(cacheEntry.Val)
+		return callback(cacheEntry.Val)
 	}
 
 	res, err := http.Get(endpoint)
@@ -84,7 +66,7 @@ func GetLocation(name string, cache *pokecache.Cache) error {
 		log.Fatal(err)
 	}
 	cache.Add(endpoint, bodyBytes)
-	return handleLocationOutput(bodyBytes)
+	return callback(bodyBytes)
 }
 
 func handleLocationsOutput(bodyBytes []byte) error {
